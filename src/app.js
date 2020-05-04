527f1f889265b4249c1f6c2b0b883ef9c68700ff
Leandro Neves <leandrosoares6@hotmail.com>
2020-05-04 18:18:26 -0300

    Challenge concluded

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,77 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  /* const parsedTechs = techs.replace(/ /, '');
+  const techsArray = parsedTechs.split(','); */
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const checkRepositoryExists = repositories.find(repository => repository.id === id);
+  
+  if (!checkRepositoryExists) {
+    return response.status(400).json('Repository not found');
+  }
+  
+  const index = repositories.indexOf(checkRepositoryExists);
+
+  repositories[index] = { ...checkRepositoryExists, title, url, techs };
+
+  return response.json(repositories[index]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const checkRepositoryExists = repositories.find(repository => repository.id === id);
+  
+  if (!checkRepositoryExists) {
+    return response.status(400).send();
+  }
+
+  const index = repositories.indexOf(checkRepositoryExists);
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const checkRepositoryExists = repositories.find(repository => repository.id === id);
+  
+  if (!checkRepositoryExists) {
+    return response.status(400).send();
+  }
+
+  const index = repositories.indexOf(checkRepositoryExists);
+
+  repositories[index].likes += 1;
+
+  return response.json({
+    likes: repositories[index].likes
+  });
 });
+
 
 module.exports = app;
